1354054022ccd1d6eec0654632d617fe85b41d29
iamvaleriebasco <iamvalerieagno655@gmail.com>
2026-04-09 01:53:23 +0800

    sc

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -6,8 +6,15 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
 
 createInertiaApp({
-    title: (title) => title ? `${title} - Bags Finals` : `Bags Finals`,
+    title: (title) => title ? `${title} - Boudoir Bags` : `Boudoir Bags`,
     resolve: (name) =>
+/**
+ * Resolves a page component by name.
+ *
+ * @param {string} name - The name of the page component to resolve.
+ *
+ * @returns {function} - A function that imports the page component.
+ */
         resolvePageComponent(
             `./Pages/${name}.jsx`,
             import.meta.glob('./Pages/**/*.jsx'),
